86ee6178876ce0f6e324d32a1de2062f3db320da
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-07-03 21:45:19 +0200

    First version

--- MakeMake/Config.cs
+++ MakeMake/Config.cs
@@ -1,5 +1,40 @@
-﻿namespace MakeMake;
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
 
+namespace MakeMake;
+
+[DataContract]
 internal class Config
 {
+    public static Config Current { get; set; }
+    public static string DirPath { get; } = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MakeMake");
+    public static string FilePath { get; } = Path.Join(DirPath, "Config.json");
+
+    static Config()
+    {
+        if (!Directory.Exists(DirPath))
+            Directory.CreateDirectory(DirPath);
+        if (!File.Exists(FilePath))
+            File.Create(FilePath);
+
+        Config? c = JsonConvert.DeserializeObject<Config>(File.ReadAllText(FilePath));
+        if (c is null)
+        {
+            Current = new();
+            File.WriteAllText(FilePath, JsonConvert.SerializeObject(Current, Formatting.Indented));
+            return;
+        }
+        Current = c;
+    }
+
+    [DataMember]
+    public string Compiler { get; set; } = "clang";
+    [DataMember]
+    public string DebugFlags { get; set; } = "-Wall -g -std=c17";
+    [DataMember]
+    public string ReleaseFlags { get; set; } = "-std=c17 -DNDEBUG -O3";
+    [DataMember]
+    public string? OutName { get; set; } = null;
+    [DataMember]
+    public string Extension { get; set; } = ".exe";
 }
--- MakeMake/Program.cs
+++ MakeMake/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MakeMake;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var f = Directory.EnumerateFiles("./").FirstOrDefault(p => p.EndsWith(".c"));
+
+        if (f is null)
+        {
+            Console.WriteLine("No C file found");
+            return;
+        }
+
+        if (File.Exists("Makefile"))
+        {
+            string? s;
+            do
+            {
+                Console.Write("Makefile already exists, do you want to override it? [Y/n]");
+                s = Console.ReadLine()?.ToLower();
+            } while (s != "y" || s != "n");
+            
+            if (s == "n")
+                return;
+        }
+
+        FileInfo fi = new(f);
+
+        string makefile =
+            "CC:=" + Config.Current.Compiler + "\n" +
+            "OUT:=" + (Config.Current.OutName ?? fi.Name[..^2]) + Config.Current.Extension + "\n" +
+            "CFLAGS:=" + Config.Current.DebugFlags + "\n" +
+            "RFLAGS:=" + Config.Current.ReleaseFlags + "\n" +
+            "CFILES:=" + fi.Name + "\n" +
+            "\n" +
+            "debug: $(CFILES)\n" +
+            "\t$(CC) $(CFLAGS) -o $(OUT) $(CFILES)\n" +
+            "\n" +
+            "release: $(CFILES)\n" +
+            "\t$(CC) $(RFLAGS) -o $(OUT) $(CFILES)\n" +
+            "\n" +
+            "clean:\n" +
+            "\tdel $(OUT)\n";
+
+        File.WriteAllText("Makefile", makefile);
+    }
+}
